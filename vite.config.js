ab13bd2b158f091fec31b68eec8c6104e94715ac
thomas schuller <thoschuller@gmail.com>
2023-01-19 20:38:46 +0000

    removed repl.deploy and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mix from 'vite-plugin-mix';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-            mix.default({
-      handler: './api.ts',
-    }),
-           ],
+  plugins: [react(),],
   server: {
     host: '0.0.0.0',
   }
